ddeed50fd8598fdf021326a0bfec7310c93d459b
VlPan <vladimirpant.bel@gmail.com>
2018-06-28 23:09:06 +0300

    bar chart

---
+++ bar/js/main.js
@@ -0,0 +1,90 @@
+
+
+
+var margin = {left: 100, right: 10, bottom: 100, top: 10};
+
+var width = 600 - margin.left - margin.right;
+var height = 400 - margin.top - margin.bottom;
+
+var data = d3.json('data/buildings.json').then(data => {
+    console.log(data);
+
+    data.forEach(d => {
+        d.height = +d.height
+    });
+
+    var svg = d3.select('#chart-area')
+    .append('svg')
+    .attr('width', width + margin.left + margin.right)
+    .attr('height', height + margin.top + margin.bottom + 140)
+
+    var g = svg.append("g")
+    .attr("transform", "translate(" + margin.left + ", " + margin.top + ")")
+
+    g.append("text")
+        .attr("class", "x axis-label")
+        .attr("x", width / 2)
+        .attr("y", height + 120)
+        .attr("font-size", "20px")
+        .attr("text-anchor", "middle")
+        .text("The world's tallest buildings!")
+    
+     g.append("text")
+        .attr("class", "x axis-label")
+        .attr("x", - (height / 2))
+        .attr("y", -60 )
+        .attr("font-size", "20px")
+        .attr("text-anchor", "middle")
+        .attr("transform", "rotate(-90)")
+        .text("Height (m)")
+   
+
+    var x = d3.scaleBand()
+        .domain(data.map(d => d.name))
+        .range([0, width])
+        .paddingInner(0.3)
+        .paddingOuter(0.3);
+    
+    var y = d3.scaleLinear()
+        .domain([0, d3.max(data, (d) => d.height)])
+        .range([height, 0])
+
+   
+   
+
+    var xAxisCall = d3.axisBottom(x);
+    g.append("g")
+    .attr("class", "x axis")
+    .attr("transform", `translate(0, ${height})`)
+    .call(xAxisCall)
+    .selectAll("text")
+        .attr("y", "10")
+        .attr("x", "-5")
+        .attr("text-anchor", "end")
+        .attr("transform", "rotate(-40)")
+
+    var yAxisCall = d3.axisLeft(y)
+        .ticks(4)
+        .tickFormat((d) => d + "m");
+    g.append('g')
+        .attr("class", "y-axis")
+        .call(yAxisCall);
+
+    var rects = g.selectAll('rect')
+        .data(data);
+
+    rects.enter()
+        .append('rect')
+
+        .attr('y', (d) => y(d.height))
+        .attr('x', (d) => x(d.name))
+        .attr('width', x.bandwidth)
+        .attr('height', (d) => height - y(d.height))
+        .attr("fill", "darkred");
+
+      
+
+});
+
+
+
