ff7c310a905469bb27009044fd5f41a6570c207c
VlPan <vladimirpant.bel@gmail.com>
2018-08-13 07:28:09 +0300

    + second project started

--- bar/js/main.js
+++ bar/js/main.js
@@ -1,7 +1,7 @@
 
 
 
-var margin = {left: 100, right: 10, bottom: 100, top: 10};
+var margin = {left: 100, right: 10, bottom: 100, top: 40};
 
 var width = 600 - margin.left - margin.right;
 var height = 400 - margin.top - margin.bottom;
@@ -81,9 +81,6 @@ var data = d3.json('data/buildings.json').then(data => {
         .attr('width', x.bandwidth)
         .attr('height', (d) => height - y(d.height))
         .attr("fill", "darkred");
-
-      
-
 });
 
 
